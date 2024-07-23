da24428b00502e966c0b766ffc42837e467bc775
marcogiabbani <mtgiabbani@gmail.com>
2024-07-23 12:34:30 -0300

    dev and prod docker imgs, docker compose, missing db

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  const MODE = process.env.NODE_ENV || 'development';
+  await app.listen(PORT, () => {
+    console.log(`Server is running in ${MODE} mode on port: ${PORT}`);
+  });
 }
 bootstrap();
