c8609210c67b029b13a2ad47dfac7f818a1b5e4b
marcogiabbani <mtgiabbani@gmail.com>
2024-07-29 16:42:38 -0300

    cleaning users module for KISS pattern

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,18 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-
-const mockUserRepository = {
-  find: jest.fn(),
-  findOne: jest.fn(),
-  create: jest.fn(),
-  save: jest.fn(),
-};
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
-  let repository: typeof mockUserRepository;
-  let mockUser: User;
+
+  const mockUserRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,31 +26,11 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
-    repository = module.get(getRepositoryToken(User));
   });
-
-  mockUser = new User('email@email.com', 'password123');
-  mockUser.id = '1';
-  mockUser.createdAt = new Date();
-  mockUser.updatedAt = new Date();
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-    expect(repository).toBeDefined();
   });
-
-  describe('create', () => {
-    it('should create a user', async () => {
-      const user: User = mockUser;
-      repository.create.mockReturnValue(user);
-      repository.save.mockResolvedValue(user);
-
-      const newUser = await service.create({
-        email: 'email@email.com',
-        password: 'password123',
-      });
-
-      expect(newUser).toBe(user);
-    });
-  });
+  //just keep a random generator for a user for each test
+  // and KISS. test on success and 1 boundary
 });
