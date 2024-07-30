2b9caa6346a4e927e7f0b8c0ce7b0d0a2e636486
marcogiabbani <mtgiabbani@gmail.com>
2024-07-30 18:39:56 -0300

    WIP waiting for services unit TDD to start on controller unit testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,38 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
-
-const mockUserRepository = {
-  find: jest.fn(),
-  findOne: jest.fn(),
-  create: jest.fn(),
-  save: jest.fn(),
-  // add more methods as needed
-};
+import { userMock } from './utils/user.mock';
 
 describe('UsersController', () => {
   let controller: UsersController;
-  let service: UsersService;
-  let repository: Repository<User>;
+
+  const usersServiceMock = {
+    findOne: jest.fn().mockResolvedValue(userMock()),
+    findAll: jest.fn().mockResolvedValue([userMock()]),
+    create: jest.fn().mockResolvedValue(userMock()),
+    update: jest.fn().mockResolvedValue(userMock()),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [
-        UsersService,
-        { provide: getRepositoryToken(User), useValue: mockUserRepository },
-      ],
-    }).compile();
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersServiceMock)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
-    service = module.get<UsersService>(UsersService);
-    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    describe('when findAll is called', () => {
+      let users: User[];
+
+      beforeEach(async () => {
+        users = await controller.findAll();
+      });
+
+      test('then it should call usersService', () => {
+        expect(usersServiceMock.findAll).toHaveBeenCalled();
+      });
+
+      test('then it should return users', () => {
+        expect(users).toEqual([userMock()]);
+      });
+    });
+  });
+
+  describe('findOne', () => {
+    describe('when findOne is called', () => {
+      let user: User;
+
+      beforeEach(async () => {
+        user = await controller.findOne(userMock().id);
+      });
+
+      //   test('then it should call usersService', () => {
+      //     expect(usersServiceMock.findAll).toHaveBeenCalled();
+      //   });
+
+      //   test('then it should return users', () => {
+      //     expect(users).toEqual([userMock()]);
+      //   });
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,33 +10,35 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  //   @Post()
+  //   create(@Body() createUserDto: CreateUserDto) {
+  //     return this.usersService.create(createUserDto);
+  //   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    const allUsers = await this.usersService.findAll();
+    return allUsers;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  //   @Patch(':id')
+  //   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //     return this.usersService.update(+id, updateUserDto);
+  //   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+  //   @Delete(':id')
+  //   remove(@Param('id') id: string) {
+  //     return this.usersService.remove(+id);
+  //   }
 }
