588cb435350e8126667ccc6057db75e88fa3f957
marcogiabbani <mtgiabbani@gmail.com>
2024-08-13 13:05:45 -0300

    adds authentication.controller unit tests

--- src/authentication/tests/authentication.controller.spec.ts
+++ src/authentication/tests/authentication.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationController } from '../authentication.controller';
 import { AuthenticationService } from '../authentication.service';
+import RequestWithUser from '../interfaces/requestWithUser.interface';
+import { userMock } from '../../users/utils/userEntity.mock';
+import { LocalAuthenticationGuard } from '../localAuthentication.guard';
+import { ExecutionContext } from '@nestjs/common';
+import { Response } from 'express';
 
 describe('AuthenticationController', () => {
   let controller: AuthenticationController;
@@ -8,7 +13,20 @@ describe('AuthenticationController', () => {
   const authenticationServiceMock = {
     register: jest.fn(),
     getAuthenticatedUser: jest.fn(),
+    getCookieWithJwtToken: jest.fn(),
   };
+
+  const requestMock = {
+    user: {
+      ...userMock(),
+    },
+  };
+
+  const responseMock = {
+    setHeader: jest.fn(),
+    send: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,6 +35,14 @@ describe('AuthenticationController', () => {
     })
       .overrideProvider(AuthenticationService)
       .useValue(authenticationServiceMock)
+      .overrideGuard(LocalAuthenticationGuard)
+      .useValue({
+        canActivate: jest.fn((context: ExecutionContext) => {
+          const request = context.switchToHttp().getRequest();
+          request.user = requestMock.user;
+          return true;
+        }),
+      })
       .compile();
 
     controller = module.get<AuthenticationController>(AuthenticationController);
@@ -33,5 +59,33 @@ describe('AuthenticationController', () => {
     });
 
     expect(authenticationServiceMock.register).toHaveBeenCalled();
+  });
+
+  describe('login', () => {
+    describe('when login is called', () => {
+      test('then it should log in the user and set a cookie', async () => {
+        //arrange
+        const jwtMock = 'eyFakeJWT';
+        authenticationServiceMock.getCookieWithJwtToken.mockReturnValue(
+          `Authentication=eyFakeJWT; HttpOnly; Path=/`,
+        );
+
+        //act
+        await controller.logIn(requestMock as RequestWithUser, responseMock);
+
+        //assert
+        expect(
+          authenticationServiceMock.getCookieWithJwtToken,
+        ).toHaveBeenCalledWith(requestMock.user.id);
+        expect(responseMock.setHeader).toHaveBeenCalledWith(
+          'Set-Cookie',
+          `Authentication=${jwtMock}; HttpOnly; Path=/`,
+        );
+        expect(responseMock.send).toHaveBeenCalledWith({
+          ...requestMock.user,
+          password: '',
+        });
+      });
+    });
   });
 });
